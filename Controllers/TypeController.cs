8993d007c6deb2ea3225f0057f1271687bd54d9b
DouglasM02 <55521413+DouglasM02@users.noreply.github.com>
2022-11-22 19:50:48 -0300

    first commit

---
+++ Controllers/TypeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Avaliacao1TecnicasDeProgramacao.Controllers
+{
+    public class TypeController : Controller
+    {
+        private readonly ILogger<TypeController> _logger;
+        public TypeController(ILogger<TypeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Type()
+        {
+            return View();
+        }
+
+        public IActionResult Sofa()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Avaliacao1TecnicasDeProgramacao.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
